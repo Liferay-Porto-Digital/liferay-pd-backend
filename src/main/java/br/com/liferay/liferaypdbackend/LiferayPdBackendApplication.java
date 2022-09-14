3d0c1ef380aa5aa7dabff9e646d7fc24bf4cfc8e
Igor Costa <ext.igor.costa@o-pitblast.com>
2022-09-14 19:54:49 -0300

    First commit

---
+++ src/main/java/br/com/liferay/liferaypdbackend/LiferayPdBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.liferay.liferaypdbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiferayPdBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiferayPdBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/liferay/liferaypdbackend/LiferayPdBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.liferay.liferaypdbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiferayPdBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
