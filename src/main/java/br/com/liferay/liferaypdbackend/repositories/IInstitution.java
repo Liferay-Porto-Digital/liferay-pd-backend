2bd6f340fa271f4de6320af81eae4309d9087398
Igor Costa <ext.igor.costa@o-pitblast.com>
2022-10-04 14:22:14 -0300

    FEAT [Added Classes Repositories]

---
+++ src/main/java/br/com/liferay/liferaypdbackend/repositories/IInstitution.java
@@ -0,0 +1,11 @@
+package br.com.liferay.liferaypdbackend.repositories;
+
+import br.com.liferay.liferaypdbackend.models.Institution;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface IInstitution extends JpaRepository<Institution, UUID> {
+}
