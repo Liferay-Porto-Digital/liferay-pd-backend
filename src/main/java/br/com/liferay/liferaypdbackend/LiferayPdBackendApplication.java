fc4f5ae2f8883f985f4efa09fa3bde811195241f
Igor Costa <ext.igor.costa@o-pitblast.com>
2022-10-04 23:36:50 -0300

    FEAT [Added]

--- src/main/java/br/com/liferay/liferaypdbackend/LiferayPdBackendApplication.java
+++ src/main/java/br/com/liferay/liferaypdbackend/LiferayPdBackendApplication.java
@@ -1,13 +1,27 @@
 package br.com.liferay.liferaypdbackend;
 
+import br.com.liferay.liferaypdbackend.models.CollaboratorModel;
+import br.com.liferay.liferaypdbackend.repositories.ICollaboratorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LiferayPdBackendApplication {
+public class LiferayPdBackendApplication implements CommandLineRunner {
+    @Autowired
+    ICollaboratorRepository collaboratorRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(LiferayPdBackendApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception
+    {
+        if (collaboratorRepository.findAll().isEmpty()) {
+            CollaboratorModel first = new CollaboratorModel("Amanda", "HR Manager", true);
+            collaboratorRepository.save(first);
+        }
+    }
 }
