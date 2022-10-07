08a4e11ca0d04e718ac0b3ce86c012489f7791ae
Igor Costa <ext.igor.costa@o-pitblast.com>
2022-10-06 23:39:00 -0300

    FEAT [Created Base Service Methods For Institution And Form]

--- src/main/java/br/com/liferay/liferaypdbackend/services/FormService.java
+++ src/main/java/br/com/liferay/liferaypdbackend/services/FormService.java
@@ -1,7 +1,12 @@
 package br.com.liferay.liferaypdbackend.services;
 
+import br.com.liferay.liferaypdbackend.models.product.FormModel;
 import br.com.liferay.liferaypdbackend.repositories.IFormRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class FormService {
@@ -11,6 +16,37 @@ public class FormService {
 
     public FormService(IFormRepository formRepository) {
         this.formRepository = formRepository;
+    }
+    //endregion
+
+    //region METHODS
+
+    /**
+     * Save form in database
+     * @param formModel
+     */
+    @Transactional
+    @Modifying
+    public void save(FormModel formModel) {
+        formRepository.save(formModel);
+    }
+
+    /**
+     * Delete/Drop form in the database
+     * @param formModel
+     */
+    @Transactional
+    @Modifying
+    public void delete(FormModel formModel) {
+        formRepository.delete(formModel);
+    }
+
+    /**
+     * Get all forms from the database
+     * @return List
+     */
+    public List<FormModel> getAllForms() {
+        return formRepository.findAll();
     }
     //endregion
 }
--- src/main/java/br/com/liferay/liferaypdbackend/services/InstitutionService.java
+++ src/main/java/br/com/liferay/liferaypdbackend/services/InstitutionService.java
@@ -1,16 +1,52 @@
 package br.com.liferay.liferaypdbackend.services;
 
+import br.com.liferay.liferaypdbackend.models.InstitutionModel;
 import br.com.liferay.liferaypdbackend.repositories.IInstitutionRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class InstitutionService {
 
     //region INJECTIONS
-    final IInstitutionRepository iInstitutionRepository;
+    final IInstitutionRepository institutionRepository;
 
     public InstitutionService(IInstitutionRepository iInstitutionRepository) {
-        this.iInstitutionRepository = iInstitutionRepository;
+        this.institutionRepository = iInstitutionRepository;
+    }
+    //endregion
+
+    //region METHODS
+
+    /**
+     * Save institution in database
+     * @param institutionModel
+     */
+    @Transactional
+    @Modifying
+    public void save(InstitutionModel institutionModel) {
+        institutionRepository.save(institutionModel);
+    }
+
+    /**
+     * Delete/Drop institution in the database
+     * @param institutionModel
+     */
+    @Transactional
+    @Modifying
+    public void delete(InstitutionModel institutionModel) {
+        institutionRepository.delete(institutionModel);
+    }
+
+    /**
+     * Get all institutions from the database
+     * @return List
+     */
+    public List<InstitutionModel> getAllForms() {
+        return institutionRepository.findAll();
     }
     //endregion
 }
