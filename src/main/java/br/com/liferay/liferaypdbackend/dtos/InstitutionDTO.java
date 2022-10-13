9f14c6eb08071452b08f2f2474bc7aa908289608
Igor Costa <ext.igor.costa@o-pitblast.com>
2022-10-13 09:49:15 -0300

    REFACTOR [Deleted Address Class]

--- src/main/java/br/com/liferay/liferaypdbackend/dtos/InstitutionDTO.java
+++ src/main/java/br/com/liferay/liferaypdbackend/dtos/InstitutionDTO.java
@@ -3,6 +3,7 @@ package br.com.liferay.liferaypdbackend.dtos;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Data
 public class InstitutionDTO {
@@ -24,4 +25,21 @@ public class InstitutionDTO {
 
     @NotBlank
     private String description;
+
+    @NotBlank
+    private String street;
+
+    @NotBlank
+    private String city;
+
+    @NotBlank
+    @Size(max = 2)
+    private String state;
+
+    @NotBlank
+    private String number;
+
+    @NotBlank
+    @Size(max = 8)
+    private String zipCode;
 }
--- src/main/java/br/com/liferay/liferaypdbackend/models/InstitutionModel.java
+++ src/main/java/br/com/liferay/liferaypdbackend/models/InstitutionModel.java
@@ -1,5 +1,6 @@
 package br.com.liferay.liferaypdbackend.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -13,14 +14,12 @@ import java.util.UUID;
 @Table(name = "InstitutionModel")
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class InstitutionModel {
     //region VARIABLES
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
-
-    @Column(nullable = false, unique = true)
-    private UUID idAddress;
 
     @Column(nullable = false)
     private String name;
@@ -42,17 +41,36 @@ public class InstitutionModel {
 
     @Column(nullable = false)
     private String description;
+
+    @Column(nullable = false)
+    private String street;
+
+    @Column(nullable = false)
+    private String city;
+
+    @Column(nullable = false, length = 2)
+    private String state;
+
+    @Column(nullable = false)
+    private String number;
+
+    @Column(nullable = false, length = 8)
+    private String zipCode;
     //endregion
 
     //region CONSTRUCTORS
-    public InstitutionModel(AddressModel addressModel, String name, String registrationNumber, String phoneNumber, String email, String url, String description) {
-        this.idAddress = addressModel.getId();
+    public InstitutionModel(String name, String registrationNumber, String phoneNumber, String email, String url, String description, String street, String city, String state, String number, String zipCode) {
         this.name = name;
         this.registrationNumber = registrationNumber;
         this.phoneNumber = phoneNumber;
         this.email = email;
         this.url = url;
         this.description = description;
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.number = number;
+        this.zipCode = zipCode;
     }
     //endregion
 }
