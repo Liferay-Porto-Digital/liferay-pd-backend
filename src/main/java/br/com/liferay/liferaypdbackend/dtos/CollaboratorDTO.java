79c7ff6ca1b8a78e9526d1170140906e41f68539
Igor Costa <ext.igor.costa@o-pitblast.com>
2022-10-24 15:47:17 -0300

    FEAT [Collaborator Endpoints Creation]: Created controller and made updates on service, repository and spring application

--- src/main/java/br/com/liferay/liferaypdbackend/dtos/CollaboratorDTO.java
+++ src/main/java/br/com/liferay/liferaypdbackend/dtos/CollaboratorDTO.java
@@ -1,18 +1,20 @@
 package br.com.liferay.liferaypdbackend.dtos;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
 
 @Data
+@AllArgsConstructor
 public class CollaboratorDTO {
 
     @NotBlank
     private String name;
 
     @NotBlank
-    private Boolean isAdministrator;
+    private String jobRole;
 
     @NotBlank
-    private String jobRole;
+    private Boolean isAdministrator;
 }
