11ad1de76ab1bb811d9aa7eccdb7628ba9161af8
mandagouveia <amanda_gouveia_@hotmail.com>
2022-11-03 22:50:19 -0300

    TEST [New Branch Created]: FormServiceTest class

---
+++ src/test/java/br/com/liferay/liferaypdbackend/services/FormServiceTest.java
@@ -0,0 +1,24 @@
+package br.com.liferay.liferaypdbackend.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FormServiceTest {
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void saveOrGetInstitution() {
+    }
+
+    @Test
+    void getCollaborator() {
+    }
+}
