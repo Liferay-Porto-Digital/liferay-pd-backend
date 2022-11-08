b5f4220d26a229a09f4980d0f07877eebd808859
icgsbr <igorcostagomessouza@gmail.com>
2022-11-08 17:47:24 -0300

    REFACTOR [Changed And Implemented Validations]

--- src/main/java/br/com/liferay/liferaypdbackend/controllers/SolicitationController.java
+++ src/main/java/br/com/liferay/liferaypdbackend/controllers/SolicitationController.java
@@ -1,14 +1,14 @@
 package br.com.liferay.liferaypdbackend.controllers;
 
 import br.com.liferay.liferaypdbackend.models.SolicitationModel;
-import br.com.liferay.liferaypdbackend.models.product.FormModel;
 import br.com.liferay.liferaypdbackend.services.SolicitationService;
+import br.com.liferay.liferaypdbackend.services.utils.ConsoleLogUtil;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.Arrays;
 
 @RestController
 @CrossOrigin(origins = "*")
@@ -26,55 +26,108 @@ public class SolicitationController {
     @GetMapping("solicitation")
     @ApiOperation(value = "Get all solicitations")
     public ResponseEntity<Object> getAllSolicitations() {
-        registerSolicitation("Get solicitation registrations");
-        return ResponseEntity.status(HttpStatus.FOUND).body(solicitationService.findAll());
+        if (!solicitationService.findAll().isEmpty()) {
+            registerSolicitation("Get solicitation registrations");
+            return ResponseEntity
+                    .status(HttpStatus.FOUND)
+                    .body(solicitationService.findAll());
+        }
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body("NOT FOUND: No solicitations registered on the database");
     }
 
     @GetMapping("solicitation/recent")
     @ApiOperation(value = "Get all solicitations ordered by the new ones")
-    public ResponseEntity<List<FormModel>> getAllRecentSolicitation() {
-        registerSolicitation("Get all solicitations ordered by newer");
-        return  ResponseEntity.status(HttpStatus.FOUND).body(solicitationService.findAllFormsOrderByNewer());
+    public ResponseEntity<Object> getAllRecentSolicitation() {
+        if (!solicitationService.findAllFormsOrderByNewer().isEmpty()) {
+            registerSolicitation("Get all solicitations ordered by newer");
+            return  ResponseEntity
+                    .status(HttpStatus.FOUND)
+                    .body(solicitationService.findAllFormsOrderByNewer());
+        }
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body("NOT FOUND: No solicitations registered on the database.");
     }
 
     @GetMapping("solicitation/older")
     @ApiOperation(value = "Get all solicitations ordered by the old ones")
-    public ResponseEntity<List<FormModel>> getAllOlderSolicitation() {
-        registerSolicitation("Get all solicitations ordered by older");
-        return ResponseEntity.status(HttpStatus.FOUND).body(solicitationService.findAllFormsAndOrderByOlder());
+    public ResponseEntity<Object> getAllOlderSolicitation() {
+        if (!solicitationService.findAllFormsAndOrderByOlder().isEmpty()) {
+            registerSolicitation("Get all solicitations ordered by older");
+            return ResponseEntity
+                    .status(HttpStatus.FOUND)
+                    .body(solicitationService.findAllFormsAndOrderByOlder());
+        }
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body("NOT FOUND: No solicitations registered on the database.");
     }
 
     @GetMapping("solicitation/collaborator/{name}")
     @ApiOperation(value = "Get all solicitations done by given collaborator")
-    public ResponseEntity<List<FormModel>> getAllSolicitationByCollaborator(@PathVariable String name) {
-        registerSolicitation("Get solicitations done by collaborator");
-        return ResponseEntity.status(HttpStatus.FOUND).body(solicitationService.findAllFormsByCollaboratorName(name));
+    public ResponseEntity<Object> getAllSolicitationByCollaborator(@PathVariable String name) {
+        if (!solicitationService.findAllFormsByCollaboratorName(name).isEmpty()) {
+            registerSolicitation("Get solicitations done by collaborator");
+            return ResponseEntity
+                    .status(HttpStatus.FOUND)
+                    .body(solicitationService.findAllFormsByCollaboratorName(name));
+        }
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body("NOT FOUND: No solicitations done by collaborator or collaborator does not exists.");
     }
 
     @GetMapping("solicitations/institution/{name}")
     @ApiOperation(value = "Get all solicitations done to given institution")
-    public ResponseEntity<List<FormModel>> getAllSolicitationByInstitution(@PathVariable String name) {
-        registerSolicitation("Get solicitations for institution");
-        return ResponseEntity.status(HttpStatus.FOUND).body(solicitationService.findAllFormsByInstitutionName(name));
+    public ResponseEntity<Object> getAllSolicitationByInstitution(@PathVariable String name) {
+        if (!solicitationService.findAllFormsByInstitutionName(name).isEmpty()) {
+            registerSolicitation("Get solicitations for institution");
+            return ResponseEntity
+                    .status(HttpStatus.FOUND)
+                    .body(solicitationService.findAllFormsByInstitutionName(name));
+        }
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body("NOT FOUND: No solicitation registered for given institution or institution does not exists.");
     }
 
     @GetMapping("solicitation/donation")
     @ApiOperation(value = "Get all donation type solicitation")
-    public ResponseEntity<List<FormModel>> getAllSolicitationTypeDonation() {
-        registerSolicitation("Get donation form");
-        return ResponseEntity.status(HttpStatus.FOUND).body(solicitationService.findAllDonationForms());
+    public ResponseEntity<Object> getAllSolicitationTypeDonation() {
+        if (!solicitationService.findAllDonationForms().isEmpty()) {
+            registerSolicitation("Get donation form");
+            return ResponseEntity
+                    .status(HttpStatus.FOUND)
+                    .body(solicitationService.findAllDonationForms());
+        }
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body("NOT FOUND: No solicitation of type donation registered on the database.");
     }
 
     @GetMapping("solicitation/activity")
     @ApiOperation(value = "Get all activity type solicitation")
-    public ResponseEntity<List<FormModel>> getAllSolicitationTypeActivity() {
-        registerSolicitation("Get activity form");
-        return ResponseEntity.status(HttpStatus.FOUND).body(solicitationService.findAllActivityForms());
+    public ResponseEntity<Object> getAllSolicitationTypeActivity() {
+        if (!solicitationService.findAllActivityForms().isEmpty()) {
+            registerSolicitation("Get activity form");
+            return ResponseEntity
+                    .status(HttpStatus.FOUND)
+                    .body(solicitationService.findAllActivityForms());
+        }
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body("NOT FOUND: No solicitation of type activity registered on the database.");
     }
 
     public void registerSolicitation(String typeOfSolicitation) {
-        SolicitationModel solicitationModel = new SolicitationModel(solicitationService.findCollaboratorByName("Maria"), typeOfSolicitation);
-        solicitationService.save(solicitationModel);
+        try {
+            SolicitationModel solicitationModel = new SolicitationModel(solicitationService.findCollaboratorByName("Maria"), typeOfSolicitation);
+            solicitationService.save(solicitationModel);
+        } catch (Exception e) {
+            ConsoleLogUtil.log.info(Arrays.toString(e.getStackTrace()));
+        }
     }
     //endregion
 }
--- src/main/java/br/com/liferay/liferaypdbackend/services/SolicitationService.java
+++ src/main/java/br/com/liferay/liferaypdbackend/services/SolicitationService.java
@@ -4,8 +4,11 @@ import br.com.liferay.liferaypdbackend.models.CollaboratorModel;
 import br.com.liferay.liferaypdbackend.models.SolicitationModel;
 import br.com.liferay.liferaypdbackend.models.product.FormModel;
 import br.com.liferay.liferaypdbackend.repositories.SolicitationRepository;
+import br.com.liferay.liferaypdbackend.services.utils.ConsoleLogUtil;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -47,11 +50,25 @@ public class SolicitationService {
     }
 
     public List<FormModel> findAllFormsByInstitutionName(String name) {
-        return solicitationRepository.findAllByInstitutionName(name);
+        try {
+            if (solicitationRepository.findAllByInstitutionName(name) != null) {
+                return solicitationRepository.findAllByInstitutionName(name);
+            }
+        } catch (Exception e) {
+            ConsoleLogUtil.log.info(Arrays.toString(e.getStackTrace()));
+        }
+        return new ArrayList<>();
     }
 
     public List<FormModel> findAllFormsByCollaboratorName(String name) {
-        return solicitationRepository.findAllByCollaboratorName(name);
+        try {
+            if (solicitationRepository.findAllByCollaboratorName(name) != null) {
+                return solicitationRepository.findAllByCollaboratorName(name);
+            }
+        } catch (Exception e) {
+            ConsoleLogUtil.log.info(Arrays.toString(e.getStackTrace()));
+        }
+        return new ArrayList<>();
     }
 
     public CollaboratorModel findCollaboratorByName(String name) {
