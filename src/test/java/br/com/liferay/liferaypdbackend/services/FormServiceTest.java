f9d62062e3cde33482be74ebbcab21521c8dd38b
icgsbr <igorcostagomessouza@gmail.com>
2022-11-09 15:28:22 -0300

    TEST [Finished FormService Unit Tests]

--- src/test/java/br/com/liferay/liferaypdbackend/services/FormServiceTest.java
+++ src/test/java/br/com/liferay/liferaypdbackend/services/FormServiceTest.java
@@ -1,8 +1,13 @@
 package br.com.liferay.liferaypdbackend.services;
 
+import br.com.liferay.liferaypdbackend.dtos.FormDTO;
+import br.com.liferay.liferaypdbackend.models.CollaboratorModel;
+import br.com.liferay.liferaypdbackend.models.InstitutionModel;
 import br.com.liferay.liferaypdbackend.models.concreteProduct.ActivityFormModel;
 import br.com.liferay.liferaypdbackend.models.product.FormModel;
+import br.com.liferay.liferaypdbackend.repositories.CollaboratorRepository;
 import br.com.liferay.liferaypdbackend.repositories.FormRepository;
+import br.com.liferay.liferaypdbackend.repositories.InstitutionRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -10,7 +15,9 @@ import org.mockito.Mock;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
@@ -19,6 +26,12 @@ import static org.mockito.Mockito.when;
 @AutoConfigureMockMvc
 class FormServiceTest {
     @Mock
+    InstitutionRepository institutionRepository;
+
+    @Mock
+    CollaboratorRepository collaboratorRepository;
+
+    @Mock
     FormRepository formRepository;
 
     @InjectMocks
@@ -26,6 +39,29 @@ class FormServiceTest {
 
     private FormModel generateActivityForm() {
         return new ActivityFormModel();
+    }
+
+    private CollaboratorModel generateCollaborator() {
+        return new CollaboratorModel (
+                "Maria",
+                "Auxiliar Administrativo",
+                true
+        );
+    }
+
+    private InstitutionModel generateInstitution() {
+        return new InstitutionModel (
+                "imip",
+                "33455",
+                "8199998888",
+                "imip@email.com",
+                "www.imip.com.br",
+                "hospital imip",
+                "Rua do Imip",
+                "Recife",
+                "PE",
+                "12345678"
+        );
     }
 
     @Test
@@ -48,10 +84,40 @@ class FormServiceTest {
     }
 
     @Test
-    void saveOrGetInstitution() {
+    void whenSaveGetOrSaveInstitution() {
+        InstitutionModel institutionModel = generateInstitution();
+        FormDTO formDTO = new FormDTO();
+        formDTO.setInstitutionName(institutionModel.getName());
+        formDTO.setObjective(new ArrayList<>());
+        formDTO.setVulnerability(new ArrayList<>());
+        formDTO.setDateOfEvent("23/05/2025");
+        formDTO.setNameContact("Igor");
+        formDTO.setLastNameContact("Costa");
+        formDTO.setValue(345.8);
+        formDTO.setInstitutionCity(institutionModel.getCity());
+        formDTO.setInstitutionEmail(institutionModel.getEmail());
+        formDTO.setInstitutionState(institutionModel.getState());
+        formDTO.setInstitutionStreet(institutionModel.getStreet());
+        formDTO.setInstitutionUrl(institutionModel.getUrl());
+        formDTO.setInstitutionPhoneNumber(institutionModel.getPhoneNumber());
+        formDTO.setInstitutionRegistrationNumber(institutionModel.getRegistrationNumber());
+        formDTO.setInstitutionZipCode(institutionModel.getZipCode());
+
+        when(institutionRepository.save(any(InstitutionModel.class))).thenReturn(institutionModel);
+        when(institutionRepository.findByName(any(String.class))).thenReturn(Optional.of(institutionModel));
+
+        Assertions.assertEquals(formDTO.getInstitutionName(), formService.saveOrGetInstitution(formDTO).getName());
+        Assertions.assertSame(InstitutionModel.class, formService.saveOrGetInstitution(formDTO).getClass());
+        Assertions.assertNotNull(formService.saveOrGetInstitution(formDTO));
     }
 
     @Test
     void getCollaborator() {
+        CollaboratorModel collaboratorModel = generateCollaborator();
+        when(collaboratorRepository.findAll()).thenReturn(Collections.singletonList(collaboratorModel));
+
+        Assertions.assertSame(CollaboratorModel.class, formService.getCollaborator().getClass());
+        Assertions.assertNotNull(formService.getCollaborator());
+        Assertions.assertEquals(collaboratorModel, formService.getCollaborator());
     }
 }
