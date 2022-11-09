b966d059c5650f76ac5522c0d8dfeb7e3a0f425d
icgsbr <igorcostagomessouza@gmail.com>
2022-11-09 08:04:18 -0300

    TEST [Implemented Unit Tests For FormService]: Task still on progress

--- src/test/java/br/com/liferay/liferaypdbackend/services/FormServiceTest.java
+++ src/test/java/br/com/liferay/liferaypdbackend/services/FormServiceTest.java
@@ -1,17 +1,50 @@
 package br.com.liferay.liferaypdbackend.services;
 
+import br.com.liferay.liferaypdbackend.models.concreteProduct.ActivityFormModel;
+import br.com.liferay.liferaypdbackend.models.product.FormModel;
+import br.com.liferay.liferaypdbackend.repositories.FormRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Collections;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 class FormServiceTest {
+    @Mock
+    FormRepository formRepository;
 
-    @Test
-    void save() {
+    @InjectMocks
+    FormService formService;
+
+    private FormModel generateActivityForm() {
+        return new ActivityFormModel();
     }
 
     @Test
-    void findAll() {
+    void whenSaveThenReturnSuccess() {
+        FormModel activityForm = generateActivityForm();
+        when(formRepository.save(any(FormModel.class))).thenReturn(activityForm);
+
+        Assertions.assertNotNull(formService.save(activityForm));
+        Assertions.assertSame(ActivityFormModel.class, formService.save(activityForm).getClass());
+    }
+
+    @Test
+    void whenFindAllThenReturnAListOfFormModel() {
+        FormModel activityForm = generateActivityForm();
+        when(formRepository.findAll()).thenReturn(Collections.singletonList(activityForm));
+
+        Assertions.assertNotNull(formService.findAll());
+        Assertions.assertEquals(1, formService.findAll().size());
+        Assertions.assertSame(ActivityFormModel.class, formService.findAll().get(0).getClass());
     }
 
     @Test
